87792585789989cc13716b56f7b36bcf7551c190
mcorrea <manuel.correa@careerbuilder.com>
2015-03-23 20:47:58 -0400

    a better version

---
+++ flatten/flatten_1.rb
@@ -0,0 +1,24 @@
+#flatten.rb
+
+def flatten(array)
+
+	if array.empty? 
+		[]
+	else
+    out=[]
+    array.each do |value|
+      if value.kind_of?(Array)
+        value.each do |newval|
+          out.push(newval)
+        end
+      else
+        out.push(value)
+      end
+    end
+		out
+	end
+end
+
+#puts flatten([[1,2,3],[],['a','p']])
+puts flatten([1,2,3])
+puts flatten([])
